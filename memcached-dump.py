475dee816338a9bb7fd7e2c78bb3e02991531bb6
boris <boris@verhovs.ky>
2020-03-11 16:11:22 -0400

    Convert from class to a simple script

--- memcached-dump.py
+++ memcached-dump.py
@@ -1,61 +1,33 @@
 import socket, re
 import sys, argparse
+import pdb
 import json
 
 
-class dump:
-    def __init__(self, **kwargs):
-        socket.setdefaulttimeout(10)
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((kwargs.get("host"), kwargs.get("port")))
+def send_cmd(cmd, s):
+    if isinstance(cmd, str):
+        cmd = cmd.encode("ascii")
 
-        # return [{'key':'key','len':4,'value':'1234'}]
+    s.send(cmd)
 
-    def __enter__(self):
-        result = []
-        items_pack = self.__send_cmd("stats items\n")
-        items = self.__unpack_items(items_pack)
-        keys = self.__unpack_keys(items)
-        for d in self.__get_data(keys):
-            result.append({"key": d[0], "len": d[1], "value": d[2]})
-        return result
+    result = bytearray()
+    while True:
+        data = s.recv(2048)
+        if not data:
+            break
+        result.extend(data)
+        # TODO: "\nEND" might be safer?
+        # TODO: why is this needed?
+        if b"END" in data:
+            break
+    # TODO: could use walrus instead:
+    # while (data := s.recv(2048)):
+    #     result.extend(data)
+    #     if b"END" in data:
+    #         break
 
-    def __exit__(self, *args):
-        return self.s.close()
-
-    def __send_cmd(self, cmd):
-        result = list()
-        self.s.send(cmd)
-        while True:
-            data = self.s.recv(2048)
-            if not data:
-                break
-            result.append(data)
-            if "END" in data:
-                break
-        return "".join(result)
-
-    def __unpack_items(self, data):
-        items = re.findall(r"STAT items:(\d+):number (\d+)", data)
-        return items
-
-    def __unpack_keys(self, data):
-        keys = list()
-        for item in data:
-            cmd = "stats cachedump " + item[0] + " " + item[1] + "\n"
-            keyStr = self.__send_cmd(cmd)
-            key = re.findall(r"ITEM ([^\s]+) \[(\d+) b", keyStr)
-            for k in key:
-                if int(k[1]) != 0:
-                    keys.append((k[0], k[1]))
-        return keys
-
-    def __get_data(self, data):
-        for key in data:
-            d = re.search(
-                "\\r\\n(?P<value>[^\\s]+)", self.__send_cmd("get " + key[0] + "\n")
-            ).groupdict()
-            yield key[0], key[1], d["value"]
+    response = result.decode("ascii")
+    return response
 
 
 if __name__ == "__main__":
@@ -65,31 +37,43 @@ if __name__ == "__main__":
         action="store",
         dest="host",
         default="127.0.0.1",
-        help="memcached server host,default[127.0.0.1]",
+        help="memcached server host, (default: 127.0.0.1)",
     )
     parser.add_argument(
         "--port",
         action="store",
         dest="port",
+        type=int,
         default="11211",
-        help="memcached server port,default[11211]",
+        help="memcached server port, (default: 11211)",
     )
+    # TODO: dump to stdout by default
     parser.add_argument(
         "--path",
         action="store",
         dest="path",
         default="/tmp/memcached.json",
-        help="File path,default[/tmp/memcached.json]",
+        help="File path, (default: /tmp/memcached.json)",
     )
     args = parser.parse_args()
-    host_port = {"host": args.host, "port": int(args.port)}
-    try:
-        with dump(**host_port) as dump_instance:
-            data = dump_instance
-    except:
-        print("can't dump data from %s:%s" % (args.host, args.port), file=sys.stderr)
-    try:
-        with open(args.path, "w") as fp:
-            json.dump(data, fp)
-    except:
-        print("can't open or write file %s" % args.path, file=sys.stderr)
+
+    socket.setdefaulttimeout(10)
+    pdb.set_trace()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((args.host, args.port))
+
+        items_pack = send_cmd(b"stats items\n", s)
+        items = re.findall(r"STAT items:(\d+):number (\d+)", items_pack)
+
+        result = []
+        for item_index, item_length in items:
+            key_str = send_cmd(f"stats cachedump {item_index} {item_length}\n", s)
+            for k, length in re.findall(r"ITEM ([^\s]+) \[(\d+) b", key_str):
+                if int(length) == 0:
+                    continue
+                value = send_cmd(f"get {k}\n", s)
+                d = re.search("\\r\\n(?P<value>[^\\s]+)", value).groupdict()
+                result.append({"key": k, "len": length, "value": d["value"]})
+
+    with open(args.path, "w") as fp:
+        json.dump(result, fp, indent=4, ensure_ascii=False)
