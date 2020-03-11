c8cedd37d2ccd0be12b7ee096128036d011a11ba
boris <boris@verhovs.ky>
2020-03-11 14:11:30 -0400

    De-camelCase

--- memcached-dump.py
+++ memcached-dump.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import socket, re
 import sys, argparse
 import json
@@ -11,20 +9,21 @@ class dump:
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((kwargs.get("host"), kwargs.get("port")))
 
-    # return [{'key':'key','len':4,'value':'1234'}]
+        # return [{'key':'key','len':4,'value':'1234'}]
+
     def __enter__(self):
         result = []
-        itemsPack = self.__sendCmd("stats items\n")
-        items = self.__unpackItems(itemsPack)
-        keys = self.__unpackKeys(items)
-        for d in self.__getData(keys):
+        items_pack = self.__send_cmd("stats items\n")
+        items = self.__unpack_items(items_pack)
+        keys = self.__unpack_keys(items)
+        for d in self.__get_data(keys):
             result.append({"key": d[0], "len": d[1], "value": d[2]})
         return result
 
     def __exit__(self, *args):
         return self.s.close()
 
-    def __sendCmd(self, cmd):
+    def __send_cmd(self, cmd):
         result = list()
         self.s.send(cmd)
         while True:
@@ -36,25 +35,25 @@ class dump:
                 break
         return "".join(result)
 
-    def __unpackItems(self, data):
+    def __unpack_items(self, data):
         items = re.findall(r"STAT items:(\d+):number (\d+)", data)
         return items
 
-    def __unpackKeys(self, data):
+    def __unpack_keys(self, data):
         keys = list()
         for item in data:
             cmd = "stats cachedump " + item[0] + " " + item[1] + "\n"
-            keyStr = self.__sendCmd(cmd)
+            keyStr = self.__send_cmd(cmd)
             key = re.findall(r"ITEM ([^\s]+) \[(\d+) b", keyStr)
             for k in key:
                 if int(k[1]) != 0:
                     keys.append((k[0], k[1]))
         return keys
 
-    def __getData(self, data):
+    def __get_data(self, data):
         for key in data:
             d = re.search(
-                "\\r\\n(?P<value>[^\\s]+)", self.__sendCmd("get " + key[0] + "\n")
+                "\\r\\n(?P<value>[^\\s]+)", self.__send_cmd("get " + key[0] + "\n")
             ).groupdict()
             yield key[0], key[1], d["value"]
 
@@ -83,10 +82,10 @@ if __name__ == "__main__":
         help="File path,default[/tmp/memcached.json]",
     )
     args = parser.parse_args()
-    hostPort = {"host": args.host, "port": int(args.port)}
+    host_port = {"host": args.host, "port": int(args.port)}
     try:
-        with dump(**hostPort) as dumpInst:
-            data = dumpInst
+        with dump(**host_port) as dump_instance:
+            data = dump_instance
     except:
         print("can't dump data from %s:%s" % (args.host, args.port), file=sys.stderr)
     try:
